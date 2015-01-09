124796b6135884827e4c2dca6ea6cfedb87ca0ed
Belf <jorge.befan@gmail.com>
2015-01-09 22:40:45 +0100

    changes 9-1-2015

--- house.php
+++ house.php
@@ -132,7 +132,7 @@
                             my_speed: 600, //animation speed
                             bg_alpha: 0.5, //background opacity
                             radius: 4, //caption rounded corner
-                            scrollImage : true, // true == image follows the page, false == image remains in the same open position
+                            scrollImage : false, // true == image follows the page, false == image remains in the same open position
                             pirobox_next : 'piro_next', // Nav buttons -> piro_next == inside piroBox , piro_next_out == outside piroBox
                             pirobox_prev : 'piro_prev',// Nav buttons -> piro_prev == inside piroBox , piro_prev_out == outside piroBox
                             close_all : '.piro_overlay, .piro_close',// add class .piro_overlay(with comma)if you want overlay click close piroBox
